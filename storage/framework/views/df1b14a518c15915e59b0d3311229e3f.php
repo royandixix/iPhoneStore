76b59ef5672da2bc46f81856c019b2243762f4a3
royandi <randiroyandi@gmail.com>
2026-05-15 05:20:23 +0800

    yes

--- storage/framework/views/df1b14a518c15915e59b0d3311229e3f.php
+++ storage/framework/views/df1b14a518c15915e59b0d3311229e3f.php
@@ -1,7 +1,7 @@
 <div class="d-flex justify-content-between align-items-center mb-4">
 
     <div>
-        <h2 class="fw-bold mb-1">Categories</h2>
+        <h2 class="mb-1">Categories</h2>
         <p class="text-muted mb-0">
             Manage iPhone categories
         </p>
